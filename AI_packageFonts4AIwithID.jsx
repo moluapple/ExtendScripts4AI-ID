95bd64c73a9180de5abdda16cad6a56ef1e8d182
animalia <UC@.(none)>
2011-08-09 10:40:51 +0800

    update filename

---
+++ AI_packageFonts4AIwithID.jsx
@@ -0,0 +1,67 @@
+﻿//http://indisnip.wordpress.com/2010/08/17/extract-metadata-with-adobe-xmp-part-2/ functions by tomaxxi
+function loadXMPLibrary() {
+    if (!ExternalObject.AdobeXMPScript) {
+        try {ExternalObject.AdobeXMPScript = new ExternalObject('lib:AdobeXMPScript'); }
+        catch (e) {alert('Unable to load the AdobeXMPScript library!'); return false; }
+    }
+    return true;
+}
+
+function unloadXMPLibrary(){
+    if ( ExternalObject.AdobeXMPScript ){
+        try {ExternalObject.AdobeXMPScript.unload(); ExternalObject.AdobeXMPScript = undefined; }
+        catch (e){alert('Unable to unload the AdobeXMPScript library!'); }
+    }
+}
+
+function getUsedFonts(doc/*获取文档使用字体*/) {
+	var
+		arr = [],
+		xmpFile, oXmp, count,
+		i, path, family, face;
+	loadXMPLibrary();
+	xmpFile = new XMPFile(doc.fsName, XMPConst.UNKNOWN, XMPConst.OPEN_FOR_READ);
+	oXmp = xmpFile.getXMP(); //Returns an XMPMeta object
+	count = oXmp.countArrayItems('http://ns.adobe.com/xap/1.0/t/pg/','xmpTPg:Fonts');
+	xmpFile.closeFile(XMPConst.CLOSE_UPDATE_SAFELY);
+	for (i = 1; i <= count; i++){
+		path = XMPUtils.composeArrayItemPath('http://ns.adobe.com/xap/1.0/t/pg/','xmpTPg:Fonts', i);
+		family = oXmp.getStructField('http://ns.adobe.com/xap/1.0/t/pg/', path, XMPConst.TYPE_FONT, 'fontFamily');
+		face =  oXmp.getStructField('http://ns.adobe.com/xap/1.0/t/pg/', path, XMPConst.TYPE_FONT, 'fontFace');		
+		arr.push(family + '\t' + face);
+	}
+	unloadXMPLibrary();
+	return arr;
+}
+
+function main() {
+	var
+		doc = File.openDialog('Select the file...', '*.ai', false),
+		docPath = doc.path,
+		arr = getUsedFonts (doc),
+		oFolder = new Folder(docPath + '/DocumentFonts'),
+		btMsg,
+		PackageFonts = function (string, oFolder) {
+			var arr = string.split(','), oPath, oFile, oName, i;
+			for (i=0; i<arr.length; i++){
+				oPath = app.fonts.itemByName(arr[i]).location;
+				oFile = File(oPath); 
+				oName = oFolder + '/' + arr[i].replace ('	', ' ').replace(' Regular', '') + 
+					oPath.substring(oPath.lastIndexOf('.'), oPath.length); 
+				oFile.copy(File(oName), true); 
+			}
+		};
+	oFolder.create(); 
+	btMsg = new BridgeTalk();
+	btMsg.target = "indesign";
+	btMsg.body = PackageFonts.toSource() + '("' + arr +'", "' + oFolder + '")';
+	btMsg.onResult = function (resultMsg) {
+		$.writeln("Result = " + resultMsg.body);
+	}
+	btMsg.onError = function (errorMsg) {
+		$.writeln("Error = " + errorMsg.body); 
+	}
+	btMsg.send();
+}
+
+main();
---
+++ ID_makeClippingMask.jsx
@@ -0,0 +1,46 @@
+﻿#targetengine 'MakeClipplingMask'
+
+var mcmTitle = '建立剪切蒙版';
+// 主进程
+// -----------------------------------------------
+function MCM() {
+	var oGroup = app.activeDocument.groups.add(app.selection),
+		clippingPath = oGroup.allPageItems[0].duplicate(oGroup.itemLayer);
+	clippingPath.bringToFront(oGroup);
+	oGroup.allPageItems[0].remove();
+	oGroup.select();
+	app.cut();
+	clippingPath.select();
+	app.pasteInto();
+}
+
+var mcmHandlers = {
+	'beforeDisplay' : function (ev) {
+		ev.target.enabled = (app.documents.length > 0 && app.selection.length > 1);
+	},		
+	'onInvoke' : function () {
+		app.doScript(MCM.toSource() + '();', ScriptLanguage.JAVASCRIPT, undefined, UndoModes.entireScript);
+	}
+};
+
+// 菜单创建
+// -----------------------------------------------
+var mcmMenuInstaller = mcmMenuInstaller ||
+	(function (mnuTitle, mnuHandlers) {
+	// 1. Create the script menu action
+		var mnuAction = app.scriptMenuActions.add(mnuTitle),
+			editMenu = app.menus.item('$ID/Main').submenus.item('$ID/&Edit'),
+			layoutMenu = app.menus[5],
+			ev;
+	
+	// 2. Attach the event listener
+		for (ev in mnuHandlers) {
+			mnuAction.eventListeners.add(ev, mnuHandlers[ev]);
+		}
+
+	// 3. Create the menu item
+		editMenu.menuItems.add(mnuAction, LocationOptions.after, editMenu.menuItems.itemByID(274));
+		layoutMenu.menuItems.add(mnuAction, LocationOptions.after, layoutMenu.menuItems.itemByID(274));
+		
+		return true;
+	})(mcmTitle, mcmHandlers);
